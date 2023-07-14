263f30c433c09f422bf2f917b71757d669bb926d
Uwah-Salome <salome140993@gmail.com>
2023-07-13 19:54:10 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - This is a description
+*puts - prints the output
+*return 0 always
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
