25cc931c35045f3bd9b3242f6cf64d0564320d67
Uwah-Salome <salome140993@gmail.com>
2023-07-15 03:18:31 -0700

    onlyNumbers_0-9_putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Enty point
+  *Description: numbers 0 to 9 in new line
+  *Return: 0 (success)
+  */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
