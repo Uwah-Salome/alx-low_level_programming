35048ef566cf8ff6564c02f4d22ce3748cbafb33
Uwah-Salome <salome140993@gmail.com>
2023-07-15 04:14:43 -0700

    Single_Numbers_with_,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,17 @@
   */
 int main(void)
 {
-	int n = 48;
+	int n;
 
-	while (n <= 57)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
-		n++;
 	}
 	putchar('\n');
 	return (0);
