2fc4c92b2c31e6067da1d3de4a516a9145b08b97
Uwah-Salome <salome140993@gmail.com>
2023-07-17 07:05:00 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,12 @@
 #include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 /**
  *main - Entry point
  *Return: 0 (success)
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - "
-			 "Dora Korpar, 2015-10-19\n";
-	int fd = open("/dev/stderr", O_WRONLY);
-
-	if (fd != -1)
-	{
-		write(fd, message, sizeof(message) - 1);
-		close(fd);
-	}
-	return (1);
+    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, strlen(message));
+    return (1);
 }
