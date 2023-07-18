876aacc168f47a066c4466875cb93d87c61b2d2c
Uwah-Salome <salome140993@gmail.com>
2023-07-18 16:58:11 -0700

    24_hours1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-  * jack_baner - Print every minute of the day of jack	* Return: ...
+  * jack_bauer - Print every minute of the day of jack	* Return: ...
   */
 void jack_bauer(void)
 {
 	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
