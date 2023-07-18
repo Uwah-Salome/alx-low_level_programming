d271b76286f89772fa381c86b12db733b10923af
Uwah-Salome <salome140993@gmail.com>
2023-07-18 12:54:57 -0700

    printIfLowcase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
   *_islower - Entry point
-  *Description: a function that checks for lowercase character.
+  *int c: a function that checks for lowercase character.
   *c: the alphabets in lower case
   *Return: 1 (success)
   */
-int _islower(int c)
+int _islower(void)
 {
+	int c;
+
 	if (c >= 97 && c <= 122)
 	{
 	return (1);
