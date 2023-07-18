5fe8e39dc9331e6104278e0b8a89915daa6135fb
Uwah-Salome <salome140993@gmail.com>
2023-07-18 14:07:00 -0700

    absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *_islower - Entry point
+  *@c: the alphabets in lower case
+  *Return: 1 (success)
+  */
+int _abs(int c)
+{
+	if (c >= 1)
+	{
+	return (c);
+	}
+	if (c == 0)
+	{
+	return (c);
+	}
+	return (c);
+}
+	
+
