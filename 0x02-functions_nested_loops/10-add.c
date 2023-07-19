bc36405dd58bd87ae0e4c65e1b213b9dcda5b08d
Uwah-Salome <salome140993@gmail.com>
2023-07-19 03:41:25 -0700

    addTwoIntegers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *add - Entry point
+  *Descrption - add two numbers
+  *@a : one of the mumbers to be added
+  *@b: the second number
+  *Return: sum (success)
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
