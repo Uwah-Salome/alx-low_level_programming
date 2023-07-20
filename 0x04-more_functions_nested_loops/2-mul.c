5184894cb6fc5bfcdfb17ee4b90f809a45a454a6
Uwah-Salome <salome140993@gmail.com>
2023-07-20 02:52:41 -0700

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *mul- Entry point
+  *Description:  a function that multiplies two integers.
+  *@a: parameter 1
+  *@b: parameter 2
+  *Return: product (success)
+  */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
