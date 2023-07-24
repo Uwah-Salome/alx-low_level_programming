afab860e7ba80aeb481ffcc34880de99e63237a4
Uwah-Salome <salome140993@gmail.com>
2023-07-24 07:01:02 -0700

    resets_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
   *reset_to_98 - Entry point
   *Description: A function updates the value it points to to 98
-  *@*n: parameter to be used
+  *@n: parameter to be used
   *Return: 0 (success)
   */
 void reset_to_98(int *n)
