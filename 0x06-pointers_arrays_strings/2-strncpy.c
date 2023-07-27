82026e341fa362aa8db6f2aceab5d2e4034bdef3
Uwah-Salome <salome140993@gmail.com>
2023-07-27 07:42:20 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *_strncpy - Entry point
+  *Description:a function that copies a string.
+  *@dest: parameter one
+  *@src: parameter two
+  *@n: parametere three
+  *Return: 0 (success)
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dest_ptr = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest_ptr = *src;
+		dest_ptr++;
+		src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest_ptr = '\0';
+		dest_ptr++;
+		n--;
+	}
+	return (dest);
+}
