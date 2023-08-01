696c934e4f105c114a0df71771181c4be0502472
Uwah-Salome <salome140993@gmail.com>
2023-07-31 19:28:20 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *_memset - Entry point
+  *Description: a function that fills memory with a constant byte.
+  *@s: parameter one
+  *@b: parameter two
+  *@n: parametere three
+  *Return: 1 (success)
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		i++;
+		s[i] = b;
+	}
+	return (s);
+}
