0bf0c183f25ef312a63dd8fd3ac04ee6a894d80d
Uwah-Salome <salome140993@gmail.com>
2023-08-02 21:25:01 -0700

    put recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "Puts with recursion";
+    	_puts_recursion(str);
+   	 return (0);
+}
