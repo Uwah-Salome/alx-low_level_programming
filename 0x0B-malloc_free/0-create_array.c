54fc6428eceb0eea00868fc5b0d9067516c9943d
Uwah-Salome <salome140993@gmail.com>
2023-08-08 05:01:55 -0700

    Array of Characters

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,12 +4,12 @@
 
 /**
   *create_array - Entry point
-  *Description:a function that creates an array of chars, 
+  *Description:a function that creates an array of chars,
   * and initializes it with a specific char.
   *@size: parameter one
   *@c: parameter two
   *Return: 0 (success)
-  */ 
+  */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
@@ -17,17 +17,17 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 	array = (char *)malloc(size * sizeof(char));
 	if (array == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	while (i < size)
 	{
 		array[i] = c;
 		i++;
 	}
-	return array;
+	return (array);
 }
