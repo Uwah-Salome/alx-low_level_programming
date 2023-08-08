92d89c47d2f29c171ccff6a5658d3b2e5f0b860c
Uwah-Salome <salome140993@gmail.com>
2023-08-08 06:25:35 -0700

    String Duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  *_strdup - Entry point
+  *Dscptn:function that returns a pointer to a newly allocated space in memory
+  *@str: parameter one
+  *Return: 0 (success)
+  */ 
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	duplicate = (char *)malloc(strlen(str) + 1);
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(duplicate, str);
+	return (duplicate);
+}
