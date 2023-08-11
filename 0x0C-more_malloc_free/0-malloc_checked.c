2268d9aafdd8e35789ba93b97c829bdd0ed9cd76
Uwah-Salome <salome140993@gmail.com>
2023-08-10 21:10:15 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,40 @@
+#include "main.h"
 #include <stdlib.h>
+#include <unistd.h>
 
+/**
+  *malloc_checked - Entry point
+  *_putchar - requirement
+  *Description: a function that allocates memory using malloc
+  *@c: The character to be written
+  *@b: input
+  *Return: ptr
+  */
 void _putchar(char c);
 
-void *malloc_checked(unsigned int b) {
-    void *ptr = malloc(b); // Allocate memory using malloc
-    
-    if (ptr == NULL) {
-        char error_message[] = "Memory allocation failed. Exiting with status 98.\n";
-        for (unsigned int i = 0; error_message[i] != '\0'; i++) {
-		 _putchar(error_message[i]);
-        }
-        exit(98); // Terminate the process with status 98
-    }
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
 
-    return ptr;
+	if (ptr == NULL)
+	{
+		char error_message[] = "Memory allocation failed. Exiting with status 98.\n";
+		int i = 0;
+
+		while (error_message[i] != '\0')
+		{
+			_putchar(error_message[i]);
+			i++;
+		}
+		exit(98);
+	}
+	return (ptr);
+}
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ */
+void _putchar(char c)
+{
+	write(1, &c, 1);
 }
